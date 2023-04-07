57af6ffe55aa8f7d6ba5443d8449489fe483fce9
nguyenanthuyen1996 <thuyennguyen19041996@gmail.com>
2023-04-07 17:52:54 +0700

    Add, delete item for todolits application

--- src/App.js
+++ src/App.js
@@ -1,25 +1,55 @@
-import logo from './logo.svg';
-import './App.css';
+import {  useRef, useState } from "react";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+var index = 0
+
+function App(){
+  const [name,setName] = useState('')
+  const [job,setJob] = useState([])
+  const inputRef =useRef()
+  const handleAdd = (e)=>{
+     e.preventDefault()
+    if(name !==''){
+      setJob([{id:index , name},...job])
+      index++
+    }
+
+    setName('')
+    inputRef.current.focus()
+
+  }
+  const handleDelete = (id)=>{
+    const delJob = job.filter((to)=>to.id !== id)
+    setJob([...delJob])
+    // setEditId(id)
+
+  }
+  const handleEdit = (id)=>{
+    const editTodo =job.find((i) =>i.id ===id)
+    setName(editTodo.name)
+
+  }
+
+
+  return(
+    <div style={{padding:30}}>
+      <input
+      ref = {inputRef}
+      value={name}
+      onChange={e =>setName(e.target.value)}
+      placeholder="Enter name..."
+      />
+      <button onClick={handleAdd}>Add</button>
+      <ul>
+        {job.map((t)=>(
+          <li key ={t.id}>{`${t.id}_${t.name}`}
+          <button onClick={() =>handleDelete(t.id)}>Delete</button>
+          <button onClick={() =>handleEdit(t.id)}>Edit</button>
+          </li>
+        ))} 
+      </ul>
     </div>
-  );
+  )
 }
 
 export default App;
+
